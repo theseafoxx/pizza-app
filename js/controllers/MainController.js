8aebd4e6b82f264bdbf1ca0c83195b346308424c
theseafoxx <tkhrabsky@gmail.com>
2015-10-30 17:29:37 +0000

    Modified controller and view per exercise instructions. Exercise complete.

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -11,7 +11,49 @@ app.controller('MainController', ['$scope', function($scope) {
       name: 'Mozzarella Sticks',
       description: 'Served with marinara sauce.',
       price: 3.95
+    },
+    {
+      name: 'Bruschetta',
+      description: 'Grilled bread garlic, tomatoes, olive oil.',
+      price: 4.95
     }
+  ];
+  
+  $scope.mains = [
+    {
+      name: 'Monalisa Chicken',
+      description: 'Chicken, ricotta, mozzarella, garlic, brown sauce',
+      price: 17.95
+    },
+    {
+      name: 'Penne a la Vodka',
+      description: 'Canadian bacon, cream, tomatoe',
+      price: 15.95
+    },
+    {
+      name: 'Grandma Pizza',
+      description: 'Garlic, basil, fresh mozzarella',
+      price: 14.95
+    }
+  ];
+  
+  $scope.extras = [
+    {
+      name: 'Broccoli rabe',
+      description: 'Sauted with garlic in oil',
+      price: 4.95
+    },
+    {
+      name: 'Garlic bread with mozzarella',
+      description: 'Awesome carbs with proteins',
+      price: 5.55
+    },
+    {
+      name: 'Fish Cheese',
+      description: 'Dough in a sauce with anchovies and lots of mozzarella. A heart attack on a plate.',
+      price: 9.95
+    }
+    
   ];
 
 }]);
